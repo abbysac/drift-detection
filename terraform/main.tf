07fc172d22736af50be57c7702e425f764705d18
camleous <camleous@yahoo.com>
2026-07-27 12:41:17 -0400

    Resolve merge conflict in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,6 @@ resource "aws_s3_bucket" "example" {
   bucket = "my-example-bucket-drift-detection"
 }
 
-<<<<<<< HEAD
 resource "aws_s3_bucket_policy" "example" {
   bucket = aws_s3_bucket.example.id
 
@@ -30,8 +29,6 @@ resource "aws_s3_bucket_policy" "example" {
   })
 }
 
-=======
->>>>>>> 4560fc03478a7107d9e4182dfe37cdc15cde97cc
 # data "aws_iam_openid_connect_provider" "github" {
 #   url = "https://token.actions.githubusercontent.com"
 
@@ -48,7 +45,6 @@ resource "aws_iam_role" "github_actions_role" {
   name = "github-actions-oidc-role"
 
   assume_role_policy = jsonencode({
-<<<<<<< HEAD
     Version = "2012-10-17",
     Statement = [
       {
@@ -64,21 +60,6 @@ resource "aws_iam_role" "github_actions_role" {
           StringEquals = {
             "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com",
             "token.actions.githubusercontent.com:sub" : "repo:abbysac@79344520/drift-detection@1307247485:ref:refs/heads/main"
-=======
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Federated = "arn:aws:iam::224761220970:oidc-provider/token.actions.githubusercontent.com"
-        }
-        Action = "sts:AssumeRoleWithWebIdentity"
-        Condition = {
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
-            "token.actions.githubusercontent.com:sub" = "repo:abbysac/drift-detection:ref:refs/heads/main"
-
->>>>>>> 4560fc03478a7107d9e4182dfe37cdc15cde97cc
           }
         }
       }
@@ -97,16 +78,12 @@ resource "aws_iam_policy" "github_actions_policy" {
         Effect = "Allow"
         Action = [
           "sts:AssumeRole",
-<<<<<<< HEAD
           "sts:AssumeRoleWithWebIdentity",
           "iam:GetRole",
           "iam:GetPolicy",
           "iam:ListAttachedRolePolicies",
           "iam:ListRolePolicies",
           "iam:GetPolicyVersion"
-=======
-          "sts:AssumeRoleWithWebIdentity"
->>>>>>> 4560fc03478a7107d9e4182dfe37cdc15cde97cc
         ]
         Resource = "*"
       }
@@ -118,7 +95,6 @@ resource "aws_iam_role_policy_attachment" "github_actions_attach" {
   role       = aws_iam_role.github_actions_role.name
   policy_arn = aws_iam_policy.github_actions_policy.arn
 }
-<<<<<<< HEAD
 
 resource "aws_iam_policy" "s3_policy" {
   name        = "s3-policy"
@@ -154,5 +130,3 @@ resource "aws_iam_role_policy_attachment" "github_s3_attach" {
   role       = aws_iam_role.github_actions_role.name
   policy_arn = aws_iam_policy.s3_policy.arn
 }
-=======
->>>>>>> 4560fc03478a7107d9e4182dfe37cdc15cde97cc
